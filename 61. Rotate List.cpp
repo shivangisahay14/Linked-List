947904b42da92f872dbce18ba2e6c735760463a6
shivangisahay14 <57834633+shivangisahay14@users.noreply.github.com>
2021-08-19 21:50:23 +0530

    Update 61. Rotate List.cpp

--- 61. Rotate List.cpp
+++ 61. Rotate List.cpp
@@ -1,4 +1,32 @@
- ListNode* rotateRight(ListNode* head, int k) {
+ListNode* rotateRight(ListNode* head, int k) {
+        // edge cases 
+        if (!head || !head->next || k == 0) return head;
+        
+        // compute the length
+        ListNode *cur = head;
+        int len = 1;
+        while (cur->next && ++len) 
+            cur = cur->next;
+        
+        // go till that node
+        cur->next = head;
+        k = k % len; 
+        k = len - k;
+        while (k--) cur = cur->next;
+        
+        // make the node head and break connection 
+        head = cur->next;
+        cur->next = NULL;
+        
+        
+        return head; 
+    }
+
+
+
+
+
+ListNode* rotateRight(ListNode* head, int k) {
         
         if(head == NULL || head -> next == NULL || k == 0) 
             return head;
